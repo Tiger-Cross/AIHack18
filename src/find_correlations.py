fa3b4bb67122d03458dd3cacd122d908dc04c64a
Jonas Scholz <jonas.scholz123@googlemail.com>
2018-11-17 19:22:09 +0000

    general version

--- src/find_correlations.py
+++ src/find_correlations.py
@@ -1,38 +1,55 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import inspect
+import os
 
-def find_educational_attainment_correlation(csv_filepath, truncation_index, education_standard = 'bachelors'):
+def find_educational_attainment_correlation(characteristic_csv_filepath, edu_attainment_csv_filepath, truncation_index = False, education_standard = 'bachelors'):
     '''
-    education_standard = 'bachelors' or 'masters'
+    education_standard = 'bachelors' or 'masters' or 'higher_education'
     '''
-    edu_attainment = pd.read_csv(r'C:\Users\Jonas\Desktop\AIhack\data\long_names\edu_attainment.csv')
-    characteristic = pd.read_csv(csv_filepath)
 
-    no_bachelors = edu_attainment["EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER: Bachelor's degree: Population 25 years and over -- (Estimate)""]
+
+
+    edu_attainment = pd.read_csv(edu_attainment_csv_filepath)
+    characteristic = pd.read_csv(characteristic_csv_filepath)
+
+    no_bachelors = edu_attainment["EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER: Bachelor's degree: Population 25 years and over -- (Estimate)"]
     no_masters = edu_attainment["EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER: Master's degree: Population 25 years and over -- (Estimate)"]
+    no_higher_ed = no_masters + no_bachelors
 
     edu_population = edu_attainment[edu_attainment.columns[3]]
     char_population = characteristic[characteristic.columns[3]]
+
+    if truncation_index == False:
+        truncation_index = len(no_masters)
 
     fractional_bachelors = no_bachelors/edu_population
     fractional_masters = no_masters/edu_population
 
     if education_standard == 'bachelors':
-        y = fractional_bachelors[0:truncation_index]
+        y = no_bachelors[0:truncation_index]
     if education_standard == 'masters':
-        y = fractional_masters[0:truncation_index]
+        y = no_masters[0:truncation_index]
+    else:
+        y = no_higher_ed[0:truncation_index]
 
     correlations = []
+    column_name = []
+
 
     for column in characteristic.columns[4:]:
-        absolute_x = characteristic[column]
-        fractional_x = absolute_x[0:truncation_index]/char_population[0:truncation_index]
-        correlations.append(np.corrcoef(fractional_x, y)[0][1])
+        absolute_x = characteristic[column][0:truncation_index]
+        fractional_x = absolute_x/char_population[0:truncation_index]
+        correlations.append(np.corrcoef(absolute_x, y)[0][1])
+        column_name.append(characteristic[column].name)
 
-        '''
-        returns max correlating factor, all correlations,
-        '''
-    return [poverty[poverty.columns[correlations.index(max(correlations))]].name, correlations]
+    df = pd.DataFrame(correlations, column_name, columns = ['correlation'])
+    '''
+    returns max correlating factor, all correlations,
+    '''
 
-find_educational_attainment_correlation(r'C:\Users\Jonas\Desktop\AIhack\data\long_names\poverty.csv', 285, education_standard = 'bachelors')
+    return df.sort_values('correlation', ascending = False)
+
+
+find_educational_attainment_correlation(r'C:\Users\Jonas\Desktop\AIhack\AIHack18\data\processed\X17_POVERTY.csv', r'C:\Users\Jonas\Desktop\AIhack\AIHack18\data\processed\X15_EDUCATIONAL_ATTAINMENT.csv', truncation_index = False, education_standard = 'higher_education')
